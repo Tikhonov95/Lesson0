9dde87a8c9103878bd2a647d56dc8b29f96eb7b0
Tikhonov95 <Tikhonovad95@gmail.com>
2024-07-02 23:57:23 +0300

    Delete homework5.py

--- homework5.py
+++
@@ -1,7 +0,0 @@
-immutable_var = (5, 10,"Alex", False)
-print(immutable_var)
-# immutable_var[1] = 15 (из-за отсутствия выделенных элементов)
-mutable_list = (5, [10,"Alex"], False)
-print(mutable_list)
-mutable_list[1][1] = 15
-print(mutable_list)
